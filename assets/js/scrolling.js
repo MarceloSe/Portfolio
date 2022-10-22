f4a734e56808577ab20ab9bb8d1bb76c7eea3d12
Marcelo Sena <marcelose15.ms@gmail.com>
2022-10-22 01:43:22 -0300

    adc projetos

--- assets/js/scrolling.js
+++ assets/js/scrolling.js
@@ -3,5 +3,6 @@ window.sr = ScrollReveal({ reset:true});
 ScrollReveal({ distance: '10px'});
 
 sr.reveal(".sobre", {duration : 2000})
+sr.reveal(".sobre-mim", {duration : 2800})
 sr.reveal(".habilidades", {duration : 1000})
 sr.reveal(".box-content", {duration : 2500})
