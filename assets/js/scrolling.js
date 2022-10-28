91f5acb9fb532a613cd405f0a746482263f8a9a5
Marcelo Sena <marcelose15.ms@gmail.com>
2022-10-28 12:05:35 -0300

    tablet mq

--- assets/js/scrolling.js
+++ assets/js/scrolling.js
@@ -1,9 +1,10 @@
 window.sr = ScrollReveal({ reset:true});
 
-ScrollReveal({ distance: '10px'});
+ScrollReveal({ distance: '5px'});
 
 sr.reveal(".sobre", {duration : 2000})
 sr.reveal(".sobre-mim", {duration : 2800})
+sr.reveal(".botoes", {duration : 2800})
 sr.reveal(".habilidades", {duration : 1000})
 sr.reveal(".box-content", {duration : 2500})
 sr.reveal(".projetos", {duration : 1000})
