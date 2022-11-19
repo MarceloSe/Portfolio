dbc71d533f496775f9525c68a18e78612a638857
Marcelo Sena <marcelose15.ms@gmail.com>
2022-11-19 19:22:44 -0300

    Update habilidades.js

--- assets/js/habilidades.js
+++ assets/js/habilidades.js
@@ -1,11 +1,5 @@
 const habilidades = document.querySelector('.habilidades')
-const h2Habilidades = habilidades.querySelectorAll('h2')
-const phabilidades = habilidades.querySelectorAll('p')
+const h2Habilidades = document.querySelectorAll('h2')
+const phabilidades = document.querySelectorAll('p')
 
-for( let valor of h2Habilidades) {
-    console.log(valor)
-}
-
-for(let vl of phabilidades) {
-    console.log(vl)
-}
+console.log(h2Habilidades).value
